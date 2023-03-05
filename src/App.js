ec1c6501b40f2609491963c9b41a275582830067
saravanan <saravananudy.webd@gmail.com>
2023-03-05 17:53:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import './Navbar.css';
+import './Home.css';
+import './About.css';
+import './Contact.css';
+import './Footer.css';
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+import About from './About';
+import Books from './Books';
+import Contact from './Contact';
+import Footer from './Footer';
+import {useState} from 'react';
+import Cart from './Cart';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
+  const[cart,setCart]=useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <BrowserRouter>
+    <Routes>
+     <Route path='/' element={<Navbar cart={cart}></Navbar>}>
+      <Route index element={<Home/>}></Route>
+      <Route path='About' element={<About/>}></Route>
+      <Route path='Books' element={<Books cart={cart} setCart={setCart}></Books>}></Route>
+      <Route path='Contact' element={<Contact/>}></Route>
+      <Route path='Cart' element={<Cart cart={cart} setcart={setCart}></Cart>}></Route>
+      <Route path='Footer' element={<Footer/>}></Route>
+     </Route>
+    </Routes>
+    </BrowserRouter>
+    {/* <Cart cart={cart} setcart={setCart}></Cart> */}
     </div>
   );
 }
